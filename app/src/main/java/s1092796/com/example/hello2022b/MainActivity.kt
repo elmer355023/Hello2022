d8e71148f95f55104973e372342b4a33dc3fcda9
Elmer <elmer355023@gmail.com>
2022-03-22 09:40:44 +0800

    第一次commit

---
+++ app/src/main/java/s1092796/com/example/hello2022b/MainActivity.kt
@@ -0,0 +1,11 @@
+package s1092796.com.example.hello2022b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
